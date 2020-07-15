a5cbbe7374928365f9f1a8a58a6f203f5826c23a
adampage1993 <63195949+adampage1993@users.noreply.github.com>
2020-07-15 09:48:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+let userScore = 0;
+let compScore = 0;
+
 // Selectors
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+const userScore_div = document.querySelector("#user-score");
+const compScore_div = document.querySelector("#comp-score");
+const result = document.querySelector("#result");
 
 // Event Listeners
 rock.addEventListener("click", function () {
   game('rock');
 });
+
 paper.addEventListener("click", function () {
   game('paper');
 });
+
 scissors.addEventListener("click", function () {
   game('scissors');
 });
@@ -22,23 +30,37 @@ function getComputerChoice() {
   return choices[randomNum];
 }
 
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_div.innerHTML = userScore;
+  result.innerHTML = `${userChoice} beats ${computerChoice}. You win!`
+}
+function lose(userChoice, computerChoice) {
+  compScore++;
+  compScore_div.innerHTML = compScore
+  result.innerHTML = `${computerChoice} beats ${userChoice}. Computer wins!`
+}
+function draw() {
+  result.innerHTML = `It's a draw!`
+}
+
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
     case 'rockscissors':
     case 'paperrock':
     case 'scissorspaper':
-      console.log('user wins');
+      win(userChoice, computerChoice);
       break;
     case 'rockpaper':
     case 'paperscissors':
     case 'scissorsrock':
-      console.log('computer wins');
+      lose(userChoice, computerChoice);
       break;
     case 'rockrock':
     case 'paperpaper':
     case 'scissorsscissors':
-      console.log('draw');
+      draw(userChoice, computerChoice);
       break;
   }
 }
